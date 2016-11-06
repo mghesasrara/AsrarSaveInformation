8ea9cb8e8813118d96e9ed514c10587ae72d6e9d
mghesasrara <theloveasrar@gmail.com>
2016-11-06 09:41:19 +0200

    6-11-2016a2

---
+++ app/src/main/java/com/mg/asrar/asrarsaveinformation/FingerPrintHandler.java
@@ -0,0 +1,10 @@
+package com.mg.asrar.asrarsaveinformation;
+
+import android.hardware.fingerprint.FingerprintManager;
+
+/**
+ * Created by user on 11/6/2016.
+ */
+public class FingerPrintHandler extends
+        FingerprintManager.AuthenticationCallback  {
+}
