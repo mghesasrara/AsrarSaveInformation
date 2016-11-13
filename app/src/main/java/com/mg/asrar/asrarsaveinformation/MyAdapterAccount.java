507da9c97a8afc1d9afa61a7d994ebca312d2a51
mghesasrara <theloveasrar@gmail.com>
2016-11-13 09:30:36 +0200

    aaa22324

--- app/src/main/java/com/mg/asrar/asrarsaveinformation/MyAdapterAccount.java
+++ app/src/main/java/com/mg/asrar/asrarsaveinformation/MyAdapterAccount.java
@@ -29,32 +29,32 @@ public class MyAdapterAccount extends ArrayAdapter<accounts> {
         convertView = LayoutInflater.from(getContext()).
                 inflate(R.layout.item_my_account, parent, false);
         TextView textView1 = (TextView) convertView.findViewById(R.id.textView1);
-       TextView textView2 = (TextView) convertView.findViewById(R.id.textView2);
-        ImageButton btndel=(ImageButton)convertView.findViewById(R.id.btndel);
+        TextView textView2 = (TextView) convertView.findViewById(R.id.textView2);
+        ImageButton btndel = (ImageButton) convertView.findViewById(R.id.btndel);
         final accounts accounts = getItem(position);
         textView1.setText(accounts.getEmail());
         textView2.setText(accounts.getPassword());
-         refernce= FirebaseDatabase.getInstance().getReference();
-         btndel.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-                 refernce.child(accounts.getId()).removeValue(new DatabaseReference.CompletionListener() {
-                     @Override
-                     public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-                         if(databaseError==null)//*delleted
-                         {
-                             Toast.makeText(getContext(),"Deleted!",Toast.LENGTH_LONG).show();
-                             remove(accounts);
-                             setNotifyOnChange(true);//*to update the list view
-                         }
-                     }
-                 })
-             }
-         });
+        refernce = FirebaseDatabase.getInstance().getReference();
+        btndel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                refernce.child(accounts.getId()).removeValue(new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                        if (databaseError == null)//*delleted
+                        {
+                            Toast.makeText(getContext(), "Deleted!", Toast.LENGTH_LONG).show();
+                            remove(accounts);
+                            setNotifyOnChange(true);//*to update the list view
+                        }
+                    }
+                });
+            }
+        });
         return convertView;
 
-
+    }
     }
 
 
-}
+
